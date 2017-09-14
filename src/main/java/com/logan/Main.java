0fa6ca2f0a33de5bbf1f5a6dd2cb6925274cab4a
loganbw <loganbryantweathersby@gmail.com>
2017-09-14 14:54:10 -0400

    added game piece's move, freeeze and unfreeze

---
+++ src/main/java/com/logan/Main.java
@@ -0,0 +1,18 @@
+package com.logan;
+
+public class Main {
+
+    public static void GamePiece(String[] args) {
+
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/logan/gamePiece.java
+++ src/main/java/com/logan/gamePiece.java
@@ -1,4 +1,60 @@
 package com.logan;
 
+import java.util.Random;
+
 public class gamePiece {
+    private boolean frozen;
+    private int postionX;
+    private int postionY;
+    private String pieceColor;
+    private String pieceName;
+    Random rng = new Random();
+
+
+    public gamePiece() {
+
+        postionX = 0;
+        postionY = 0;
+        frozen = false;
+    }
+    public void move(){
+        if(!frozen){
+             postionX = rng.nextInt(100) + 1;
+             postionY = rng.nextInt(100) + 1;
+        }
+    }
+    public void freeze(){
+        frozen = true;
+    }
+    public void unfreeze(){
+        frozen = false;
+    }
+
+    public boolean isFrozen() {
+        return frozen;
+    }
+
+    public int getPostionX() {
+        return postionX;
+    }
+
+    public int getPostionY() {
+        return postionY;
+    }
+
+    public String getPieceColor() {
+        return pieceColor;
+    }
+
+    public void setPieceColor(String pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public String getPieceName() {
+        return pieceName;
+    }
+
+    public void setPieceName(String pieceName) {
+        this.pieceName = pieceName;
+    }
 }
